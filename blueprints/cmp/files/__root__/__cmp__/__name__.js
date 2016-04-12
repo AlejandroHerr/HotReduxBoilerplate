2d860d8ab64c05035f2156168833ab2dbe40572f
AlejandroHerr <alejandrohnc88@gmail.com>
2016-04-12 17:55:42 +0200

    redux-cli

---
+++ blueprints/cmp/files/__root__/__cmp__/__name__.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import Component from 'components/PureComponent';
+
+export default class <%= pascalEntityName %> extends Component {
+  render() {
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+<%= pascalEntityName %>.defaultProps = {
+};
+
+<%= pascalEntityName %>.propTypes = {
+};
